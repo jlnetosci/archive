779e2b195bf26d2638b726452583b079c87668c7
jlnetosci <jlnetosci@gmail.com>
2023-07-07 18:17:46 +0100

    Initial commit

---
+++ summary_table.R
@@ -0,0 +1,23 @@
+library(knitr)
+
+system('paste original.lst wayback.lst pages.lst | tr "\t" "," > tmp.csv')
+
+table <- "tmp.csv" |>
+  read.csv(header = FALSE) |>
+  setNames(c("original", "wayback", "page")) |>
+  knitr::kable()
+
+readme <- readLines("README.md")
+
+if (length(grep("\\|", readme)) > 0){
+  #if there is already a table: replace it
+  table_start_index <- min(grep("^\\|", readme))
+  readme[table_start_index:(table_start_index + length(table) - 1)] <- table
+} else {
+  #if there is no table: create first table
+  readme <- c(readme, "", "## Summary table", "", table)
+}
+
+writeLines(readme, "README.md")
+
+system('rm tmp.csv')
